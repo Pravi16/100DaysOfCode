3cf7a62a84508bd4e8a0e5dbe43f0ffee93b17b5
Pravi16 <ngk.praveenkumar@gmail.com>
2022-06-19 23:07:59 +0530

    Day 10

---
+++ Tree/leftview.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void compute(TreeNode *root,int level,vector<int>&ans)
+    {
+        if(!root) return;
+        if(ans.size()==level) ans.push_back(root->val);
+        compute(root->left,level+1,ans);
+        compute(root->right,level+1,ans);
+    }
+    
+    vector<int> leftView(TreeNode *root) 
+    {
+        vector<int>ans;
+        compute(root,0,ans);
+        return ans;
+    }
+};
