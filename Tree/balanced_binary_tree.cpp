da3db80ba3e3730a7f6cdb76c5a6d161f4ff8b8a
Pravi16 <ngk.praveenkumar@gmail.com>
2022-06-21 14:13:53 +0530

    Day 12

---
+++ Tree/balanced_binary_tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    
+    int depth(TreeNode *root)
+    {
+        if(!root) return 0;
+        int lh=depth(root->left);
+        if(lh==-1) return -1;
+        int rh=depth(root->right);
+        if(rh==-1) return -1;
+        if(abs(lh-rh)>1) return -1;
+        return 1+max(lh,rh);
+    }
+    bool isBalanced(TreeNode* root) {
+        return depth(root)!=-1;
+
+    }
+};
