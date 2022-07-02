c3996233f0b52963cd37a8048012a01e46e2e550
Pravi16 <ngk.praveenkumar@gmail.com>
2022-07-02 21:50:16 +0530

    Day 23

---
+++ String/group_anagrams.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        map<string,vector<string>>mp;
+        for(int i=0;i<strs.size();i++)
+        {
+            string temp=strs[i];
+            sort(temp.begin(),temp.end());
+            mp[temp].push_back(strs[i]);
+        }
+        for(auto i:mp) ans.push_back(i.second);
+        return ans;
+    }
+};
