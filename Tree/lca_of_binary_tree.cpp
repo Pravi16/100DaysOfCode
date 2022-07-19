b645900652ec3acc92223deb6c3dcaaae8bf4758
Pravi16 <ngk.praveenkumar@gmail.com>
2022-07-19 18:40:53 +0530

    Day 40

---
+++ Tree/lca_of_binary_tree.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root) return root;
+        if(root->val < p->val and root->val < q->val) return lowestCommonAncestor(root->right,p,q);
+        if(root->val > p->val and root->val > q->val) return lowestCommonAncestor(root->left,p,q);
+        return root;
+    }
+};
