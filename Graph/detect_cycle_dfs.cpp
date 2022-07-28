eaf9e070921f562ea9a884da09af0b9973b13110
Pravi16 <ngk.praveenkumar@gmail.com>
2022-07-28 12:18:55 +0530

    Day 49

---
+++ Graph/detect_cycle_dfs.cpp
@@ -0,0 +1,30 @@
+
+
+class Solution {
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool compute(int cur,int prev,vector<int>adj[],vector<int>&vis)
+    {
+        vis[cur]=1;
+        for(auto i:adj[cur])
+        {
+            if(!vis[i])
+            {
+                if(compute(i,cur,adj,vis)) return true;
+            }
+            else if(i!=prev) return true;
+        }
+        return false;
+    }
+    bool isCycle(int V, vector<int> adj[]) {
+        vector<int>vis(V0); // starting with 0
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i])
+            {
+                if(compute(i,-1,adj,vis)) return true;
+            }
+        }
+        return false;
+    }
+};
